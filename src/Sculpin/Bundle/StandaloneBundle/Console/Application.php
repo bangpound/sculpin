b1620ca7d139a1943bae436ff622a8dcc9bdff66
Benjamin Doherty <bjd@bangpound.org>
2015-07-11 16:52:06 -0500

    Move Application class to StandaloneBundle.

--- src/Sculpin/Bundle/StandaloneBundle/Console/Application.php
+++ src/Sculpin/Bundle/StandaloneBundle/Console/Application.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Sculpin\Bundle\SculpinBundle\Console;
+namespace Sculpin\Bundle\StandaloneBundle\Console;
 
 use Dflydev\EmbeddedComposer\Core\EmbeddedComposerAwareInterface;
 use Dflydev\EmbeddedComposer\Core\EmbeddedComposerInterface;
