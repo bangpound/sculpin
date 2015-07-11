15c672ff757667f4c0d06cab4de389454bd9c3f5
Benjamin Doherty <bjd@bangpound.org>
2015-07-11 16:41:59 -0500

    framework.xml contains Symfony Framework bundle definitions.

--- src/Sculpin/Bundle/StandaloneBundle/DependencyInjection/SculpinStandaloneExtension.php
+++ src/Sculpin/Bundle/StandaloneBundle/DependencyInjection/SculpinStandaloneExtension.php
@@ -29,6 +29,6 @@ class SculpinStandaloneExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader->load('framework.xml');
     }
 }
