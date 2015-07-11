ea37cf4f7e3e3dd3abe1f91e81ea5ab8b6c7b49e
Benjamin Doherty <bjd@bangpound.org>
2015-07-11 18:02:44 -0500

    use original embedded composer commands.
    * `sculpin:` prefix is configured by default.
    * StandaloneBundle compiler pass removes the prefix on those commands.

--- src/Sculpin/Bundle/StandaloneBundle/SculpinStandaloneBundle.php
+++ src/Sculpin/Bundle/StandaloneBundle/SculpinStandaloneBundle.php
@@ -12,6 +12,7 @@
 namespace Sculpin\Bundle\StandaloneBundle;
 
 use Sculpin\Bundle\StandaloneBundle\DependencyInjection\Compiler\AddConsoleCommandPass;
+use Sculpin\Bundle\StandaloneBundle\DependencyInjection\Compiler\ModifyComposerCommandPass;
 use Sculpin\Bundle\StandaloneBundle\DependencyInjection\Compiler\RegisterKernelListenersPass;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -27,6 +28,7 @@ class SculpinStandaloneBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         $container->addCompilerPass(new AddConsoleCommandPass());
+        $container->addCompilerPass(new ModifyComposerCommandPass());
         $container->addCompilerPass(new RegisterKernelListenersPass, PassConfig::TYPE_AFTER_REMOVING);
     }
 }
