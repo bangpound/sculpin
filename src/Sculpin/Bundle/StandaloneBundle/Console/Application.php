58478d3b05d5ec7f15356d8548acd3f1f04aa052
Benjamin Doherty <bjd@bangpound.org>
2015-07-12 04:51:16 -0500

    add commands more like framework bundle's application.

--- src/Sculpin/Bundle/StandaloneBundle/Console/Application.php
+++ src/Sculpin/Bundle/StandaloneBundle/Console/Application.php
@@ -25,6 +25,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\HttpKernel\KernelInterface;
 
@@ -38,6 +39,8 @@ class Application extends BaseApplication implements EmbeddedComposerAwareInterf
     protected $kernel;
     protected $embeddedComposer;
     
+    private $commandsRegistered = false;
+
     /**
      * Constructor.
      *
@@ -108,8 +111,20 @@ class Application extends BaseApplication implements EmbeddedComposerAwareInterf
             $this->add(new SelfUpdateCommand(''));
             $this->add(new UpdateCommand(''));
         } else {
-            $this->registerCommands();
+            if (!$this->commandsRegistered) {
+                $this->registerCommands();
+
+                $this->commandsRegistered = true;
+            }
+
             $container = $this->kernel->getContainer();
+
+            foreach ($this->all() as $command) {
+                if ($command instanceof ContainerAwareInterface) {
+                    $command->setContainer($container);
+                }
+            }
+
             $this->setDispatcher($container->get('event_dispatcher'));
         }
 
