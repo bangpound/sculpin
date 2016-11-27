0ca006b10009e83c64410c46b7701aa142e29559
Benjamin Doherty <bjd@bangpound.org>
2016-11-27 10:30:14 -0600

    remove embedded composer.

--- src/Sculpin/Bundle/SculpinBundle/Console/Application.php
+++ src/Sculpin/Bundle/SculpinBundle/Console/Application.php
@@ -11,13 +11,7 @@
 
 namespace Sculpin\Bundle\SculpinBundle\Console;
 
-use Dflydev\EmbeddedComposer\Core\EmbeddedComposer;
-use Dflydev\EmbeddedComposer\Core\EmbeddedComposerAwareInterface;
 use Sculpin\Core\Sculpin;
-use Sculpin\Bundle\SculpinBundle\Command\DumpAutoloadCommand;
-use Sculpin\Bundle\SculpinBundle\Command\InstallCommand;
-use Sculpin\Bundle\SculpinBundle\Command\SelfUpdateCommand;
-use Sculpin\Bundle\SculpinBundle\Command\UpdateCommand;
 use Symfony\Component\Console\Application as BaseApplication;
 use Symfony\Component\Console\Formatter\OutputFormatter;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
@@ -34,30 +28,24 @@ use Symfony\Component\HttpKernel\KernelInterface;
  *
  * @author Beau Simensen <beau@dflydev.com>
  */
-class Application extends BaseApplication implements EmbeddedComposerAwareInterface
+class Application extends BaseApplication
 {
     protected $kernel;
-    protected $embeddedComposer;
     
     /**
      * Constructor.
      *
      * @param KernelInterface  $kernel           A KernelInterface instance
-     * @param EmbeddedComposer $embeddedComposer Composer Class Loader
      */
-    public function __construct(KernelInterface $kernel, EmbeddedComposer $embeddedComposer)
+    public function __construct(KernelInterface $kernel)
     {
         $this->kernel = $kernel;
-        $this->embeddedComposer = $embeddedComposer;
 
         if (function_exists('date_default_timezone_set') && function_exists('date_default_timezone_get')) {
             date_default_timezone_set(@date_default_timezone_get());
         }
 
-        $version = $embeddedComposer->findPackage('sculpin/sculpin')->getPrettyVersion();
-        if ($version !== Sculpin::GIT_VERSION && Sculpin::GIT_VERSION !== '@'.'git_version'.'@') {
-            $version .= ' ('.Sculpin::GIT_VERSION.')';
-        }
+        $version = ' ('.Sculpin::GIT_VERSION.')';
 
         parent::__construct('Sculpin', $version.' - '.$kernel->getName().'/'.$kernel->getEnvironment().($kernel->isDebug() ? '/debug' : ''));
 
@@ -66,14 +54,6 @@ class Application extends BaseApplication implements EmbeddedComposerAwareInterf
         $this->getDefinition()->addOption(new InputOption('--no-debug', null, InputOption::VALUE_NONE, 'Switches off debug mode.'));
         $this->getDefinition()->addOption(new InputOption('--safe', null, InputOption::VALUE_NONE, 'Enable safe mode (no bundles loaded, no kernel booted)'));
         $this->getDefinition()->addOption(new InputOption('--git-version', null, InputOption::VALUE_NONE, 'See Git version'));
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getEmbeddedComposer()
-    {
-        return $this->embeddedComposer;
     }
 
     /**
@@ -107,10 +87,6 @@ class Application extends BaseApplication implements EmbeddedComposerAwareInterf
         if ($input->hasParameterOption('--safe')) {
             // For safe mode we should enable some commands
             // manually because we won't enable any others.
-            $this->add(new DumpAutoloadCommand(''));
-            $this->add(new InstallCommand(''));
-            $this->add(new SelfUpdateCommand(''));
-            $this->add(new UpdateCommand(''));
         } else {
             $this->registerCommands();
         }
